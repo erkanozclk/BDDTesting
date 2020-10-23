84448ea03bcfb3c14b1b5f299fe0c38f4bf08dfd
erkanozcelik19@gmail.com <Karpuz.47>
2020-10-22 21:22:31 -0400

    First commit

---
+++ src/test/java/practice1/Erkan.java
@@ -0,0 +1,19 @@
+package practice1;
+
+public class Erkan {
+
+
+    /*******
+     * 1. Clone repository and create a branch under your name.
+     * 2. Create a new class with your name under practice1
+     * 3. Create main method and print out something.
+     * 4. Create pull request.
+     *
+     * NOT: Please remember before you create pull request pull from master to not broke anybody code.
+     *
+     * *****/
+
+
+
+
+}
